03c75fc0ea965001a06b8f81a6555b8906ffa911
xfstc <lfan@DESKTOP-M8NRRNC.localdomain>
2022-01-19 11:23:41 -0500

    Add authentication

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
+const passport = require('passport');
+const authorization = require('./authorization');
 
 // version and author from our package.json file
 //const { version, author } = require('../package.json');
@@ -29,9 +31,9 @@ app.use(cors());
 // Use gzip/deflate compression middleware
 app.use(compression());
 
-// modifications to src/app.js
-
-// Remove `app.get('/', (req, res) => {...});` and replace with:
+// Set up our passport authorization middleware
+passport.use(authorization.strategy());
+app.use(passport.initialize());
 
 // Define our routes
 app.use('/', require('./routes'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,10 @@
-// // Define a simple health check route. If the server is running
-// // we'll respond with a 200 OK.  If not, the server isn't healthy.
-// app.get('/', (req, res) => {
-//   // Clients shouldn't cache this response (always request it fresh)
-//   // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-//   res.setHeader('Cache-Control', 'no-cache');
-
-//   // Send a 200 'OK' response with info about our repo
-//   res.status(200).json({
-//     status: 'ok',
-//     author,
-//     githubUrl: 'https://github.com/xfstc/fragments',
-//     version,
-//   });
-// });
-
 // src/routes/index.js
 
 const express = require('express');
 
 // version and author from package.json
 const { version, author } = require('../../package.json');
+const { authenticate } = require('../authorization');
 
 // Create a router that we can use to mount our API
 const router = express.Router();
@@ -27,7 +12,7 @@ const router = express.Router();
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  */
-router.use(`/v1`, require('./api'));
+router.use(`/v1`, authenticate(), require('./api'));
 
 /**
  * Define a simple health check route. If the server is running
