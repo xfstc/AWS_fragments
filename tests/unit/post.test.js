ff8e8cd9c2521bf390c53b410e3329b14bddda97
xfstc <lfan@DESKTOP-M8NRRNC.localdomain>
2022-02-27 11:47:30 -0500

    Add supported types

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -25,8 +25,7 @@ describe('POST /v1/fragments', () => {
     const res = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
-      .set('content-type', 'application/json')
-      .send({ fragment: 'fragment' });
+      .set('content-type', 'image/png');
     expect(res.statusCode).toBe(415);
   });
 });
