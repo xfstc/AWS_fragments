b9a90c381e51cb7c68d255a6d03a2e2a355751a2
xfstc <lfan@DESKTOP-M8NRRNC.localdomain>
2022-04-17 13:42:25 -0400

    Simplify conditional logic

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -9,23 +9,13 @@ const { Fragment } = require('../../model/fragment');
 module.exports = async (req, res) => {
   var expand = req.url.search('expand=1');
   try {
-    if (expand > 0) {
-      var fragment = await Fragment.byUser(req.user, true);
+    var fragment = await Fragment.byUser(req.user, expand > 0);
 
-      res.status(200).json(
-        createSuccessResponse({
-          fragment,
-        })
-      );
-    } else {
-      var fragments = await Fragment.byUser(req.user);
-
-      res.status(200).json(
-        createSuccessResponse({
-          fragments,
-        })
-      );
-    }
+    res.status(200).json(
+      createSuccessResponse({
+        fragment,
+      })
+    );
   } catch (err) {
     res.status(404).json(createErrorResponse(404, err));
   }
--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -12,14 +12,6 @@ describe('GET /v1/fragments', () => {
   test('incorrect credentials are denied', () =>
     request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
-  // Using a valid username/password pair should give a success result with a .fragments ids array
-  test('authenticated users get a fragments array', async () => {
-    const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
-    expect(res.statusCode).toBe(200);
-    expect(res.body.status).toBe('ok');
-    expect(Array.isArray(res.body.fragments)).toBe(true);
-  });
-
   // when expand = 1, all fragments including representations will be returned
   test('when expand = 1, returns all fragments including representations', async () => {
     const res = await request(app)
